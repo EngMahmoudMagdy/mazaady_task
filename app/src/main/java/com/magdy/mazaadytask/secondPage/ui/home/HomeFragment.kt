89ad3a08415f2264a257b4ae1af3c4ea8b6c6977
MahmoudMagdyKoinz <mm19>
2024-01-18 17:44:26 +0200

    Final change for Second Page

--- app/src/main/java/com/magdy/mazaadytask/secondPage/ui/home/HomeFragment.kt
+++ app/src/main/java/com/magdy/mazaadytask/secondPage/ui/home/HomeFragment.kt
@@ -4,20 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSmoothScroller
-import androidx.recyclerview.widget.PagerSnapHelper
-import androidx.recyclerview.widget.RecyclerView
-import com.magdy.mazaadytask.R
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import com.google.android.material.tabs.TabLayoutMediator
 import com.magdy.mazaadytask.databinding.FragmentHomeBinding
+import kotlin.math.abs
 
 class HomeFragment : Fragment() {
 
-    private lateinit var layoutManager: LinearLayoutManager
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -29,13 +24,8 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this)[HomeViewModel::class.java]
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -47,53 +37,45 @@ class HomeFragment : Fragment() {
         val items = listOf(1, 2, 3, 4)
         val adapter = SliderAdapter(items)
         slider.adapter = adapter
-        layoutManager = ProminentLayoutManager(requireContext())
-        slider.layoutManager = layoutManager
-        createDashIndicator(items.size)
+        slider.offscreenPageLimit = 3
+        slider.setPageTransformer(addTransformer())
+
+        TabLayoutMediator(indicatorLayout, slider) { tab, position ->
+            //Some implementation
+        }.attach()
+
+        val storyAdapter = StoryAdapter(listOf(1, 2, 3, 4, 5, 6, 7))
+        storiesRecycler.adapter = storyAdapter
     }
 
-    private fun updateIndicator(position: Int) {
-        for (i in 0 until binding.indicatorLayout.childCount) {
-            val indicator = binding.indicatorLayout.getChildAt(i)
-            indicator.setBackgroundResource(
-                if (i == position) R.drawable.selected_indicator else R.drawable.unselected_indicator
-            )
+    private fun addTransformer(): CompositePageTransformer {
+        return CompositePageTransformer().apply {
+            addTransformer { page, position ->
+                val offset = 30
+                when {
+                    position < -1 -> { // Page is off to the left
+                        page.translationX = 0f
+                        page.scaleX = 1f
+                        page.scaleY = 1f
+                    }
+                    position <= 0 -> { // Page is in the process of sliding in
+                        page.translationX = -offset * position
+                        page.scaleX = 1f
+                        page.scaleY = 1f
+                    }
+                    position <= 1 -> { // Page is in the process of sliding out
+                        page.translationX = offset * -position
+                        page.scaleX = 1f
+                        page.scaleY = 1f
+                    }
+                    else -> { // Page is off to the right
+                        page.translationX = 0f
+                        page.scaleX = 1f
+                        page.scaleY = 1f
+                    }
+                }
+            }
         }
     }
 
-    private fun createDashIndicator(count: Int) {
-        val indicatorLayout = binding.indicatorLayout
-        for (i in 0 until count) {
-            val indicator = View(requireContext())
-            indicator.setBackgroundResource(R.drawable.unselected_indicator)
-            val params = LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-            params.setMargins(8, 0, 0, 0)
-            indicator.layoutParams = params
-            indicatorLayout.addView(indicator)
-        }
-    }
-
-    private fun initRecyclerViewPosition(position: Int) {
-        // This initial scroll will be slightly off because it doesn't
-        // respect the SnapHelper. Do it anyway so that the target view
-        // is laid out, then adjust onPreDraw.
-        val snapHelper = PagerSnapHelper()
-
-        layoutManager.scrollToPosition(position)
-
-        binding.slider.doOnPreDraw {
-            val targetView = layoutManager.findViewByPosition(position)
-                ?: return@doOnPreDraw
-
-            val distanceToFinalSnap =
-                snapHelper.calculateDistanceToFinalSnap(layoutManager, targetView)
-                    ?: return@doOnPreDraw
-
-            layoutManager.scrollToPositionWithOffset(position, -distanceToFinalSnap[0])
-
-        }
-    }
-    }
+}
